f05314c29d9f0deec6b4d0dbbc9e1029804c9583
Thomas Elvin <thom855j@cvkweb.dk>
2015-08-29 23:59:05 +0200

    Create class
    Create class

---
+++ Cookie.php
@@ -0,0 +1,35 @@
+<?php
+namespace thom855j\security;
+
+class Cookie
+{
+
+    public static
+            function exists($name)
+    {
+        return (isset($_COOKIE[$name])) ? true : false;
+    }
+
+    public static
+            function get($name)
+    {
+        return $_COOKIE[$name];
+    }
+
+    public static
+            function set($name, $value, $expiry)
+    {
+        if (setcookie($name, $value, time() + $expiry, '/'))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static
+            function delete($name)
+    {
+        self::set($name, '', time() - 1);
+    }
+
+}
